ca4d106cf3ed8c8e629c21a3ba06b5b61270a99a
selalmaz <ahmetsllmz@gmail.com>
2024-08-07 10:35:23 +0300

    pre commit denemee

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 function App() {
   return (
     <SafeAreaView>
       <Text>amsasmo</Text>
+
       <Text>dasdasd</Text>
+
+      <View style={{backgroundColor: 'red', flex: 1}}></View>
     </SafeAreaView>
   );
 }
