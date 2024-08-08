bd8ae4f7307233bf97f81e39572746ea5711426c
selalmaz <ahmetsllmz@gmail.com>
2024-08-08 10:07:00 +0300

    bottom nav eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import {Provider} from 'react-redux';
 import {Store} from './redux/Store';
 import First from './First/First';
 import Second from './Second/Second';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <Provider store={Store}>
-      <SafeAreaView style={{flex: 1}}>
-        <First></First>
-        <Second></Second>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="inputPage" component={First}></Tab.Screen>
+          <Tab.Screen name="outputPage" component={Second}></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
